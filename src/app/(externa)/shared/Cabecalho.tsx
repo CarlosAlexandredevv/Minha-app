c3cdbcbe29be58c2c4f4966fe7aee4c2ca789d50
Carlos Alexandre <alexfilho062005@gmail.com>
2025-02-05 14:21:08 -0300

    :lipstick: feat: menu style

--- src/app/(externa)/shared/Cabecalho.tsx
+++ src/app/(externa)/shared/Cabecalho.tsx
@@ -1,10 +1,12 @@
 import Logo from '@/app/shared/Logo';
+import Menu from './Menu';
 
 export default function Cabecalho() {
   return (
     <header className="flex justify-center items-center h-[64px] fixed top-0 left-0 right-0 border-b border-zinc-800 bg-black opacity-80 backdrop-blur-sm">
       <div className="flex items-center w-[1400px] h-full gap-12">
         <Logo />
+        <Menu />
       </div>
     </header>
   );

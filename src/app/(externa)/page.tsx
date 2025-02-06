6ce34482c9bdb53b17c8f53fc76d204c20dda750
Carlos Alexandre <alexfilho062005@gmail.com>
2025-02-06 13:56:52 -0300

    :lipstick: feat: style titulo

--- src/app/(externa)/page.tsx
+++ src/app/(externa)/page.tsx
@@ -1,9 +1,14 @@
 import Principal from './landing/Principal';
+import Titulo from './shared/Titulo';
 
 export default function Page() {
   return (
     <div className="flex flex-col">
       <Principal />
+      <Titulo
+        texto="Nossos Recursos"
+        legenda="Lorem ipsum dolor sit amet, consectetur "
+      />
     </div>
   );
 }

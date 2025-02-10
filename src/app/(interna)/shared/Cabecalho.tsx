421870db3a6e6f0e8ff1040431edd8cb8dbc1d04
Carlos Alexandre <alexfilho062005@gmail.com>
2025-02-10 18:23:22 -0300

    :lipstick: feat: styles menu usuario

--- src/app/(interna)/shared/Cabecalho.tsx
+++ src/app/(interna)/shared/Cabecalho.tsx
@@ -5,7 +5,11 @@ export default function Cabecalho() {
   return (
     <header className="flex items-center justify-between px-7 h-20 bg-zinc-950">
       <IconMenu2 className="text-zinc-400 hover:text-zinc-200 cursor-pointer duration-300" />
-      <MenuUsuario />
+      <MenuUsuario
+        nome="Usuário Mock"
+        email="usuario@zmail.com"
+        imagem="https://picsum.photos/70/70?grayscale"
+      />
     </header>
   );
 }

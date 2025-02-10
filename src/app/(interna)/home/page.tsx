141aff27c644aa7045c3939dda929b4ee0b74f2b
Carlos Alexandre <alexfilho062005@gmail.com>
2025-02-10 18:36:21 -0300

    :lipstick: feat: style titulo

--- src/app/(interna)/home/page.tsx
+++ src/app/(interna)/home/page.tsx
@@ -1,3 +1,13 @@
+import Titulo from '../shared/Titulo';
+
 export default function Page() {
-  return <div>Página Interna</div>;
+  return (
+    <div>
+      <Titulo
+        texto="Área do Aluno"
+        legenda="Lista de todos os exercícios implementados"
+      />
+      <div className="py-10">Pagina interna</div>
+    </div>
+  );
 }

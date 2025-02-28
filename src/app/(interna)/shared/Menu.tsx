1ee36bd9c2b17e78fa09427ca75b9297bc3fe65e
Carlos Alexandre <alexfilho062005@gmail.com>
2025-02-28 15:40:49 -0300

    :sparkles: feat erro

--- src/app/(interna)/shared/Menu.tsx
+++ src/app/(interna)/shared/Menu.tsx
@@ -1,14 +1,33 @@
+'use client';
+
 import { IconBug, IconLayoutDashboard, IconLoader } from '@tabler/icons-react';
 import MenuItem from './MenuItem';
 import MenuGrupo from './MenuGrupo';
+import { usePathname } from 'next/navigation';
 
 export default function Menu() {
+  const url = usePathname();
   return (
     <nav className="flex flex-col gap-4">
-      <MenuItem url="/home" texto="Início" icone={<IconLayoutDashboard />} />
+      <MenuItem
+        url="/home"
+        texto="Início"
+        icone={<IconLayoutDashboard />}
+        selecionado={url === '/home'}
+      />
       <MenuGrupo titulo="Exercícios">
-        <MenuItem url="/loading" texto="Loading" icone={<IconLoader />} />
-        <MenuItem url="/erro" texto="Erro" icone={<IconBug />} />
+        <MenuItem
+          url="/loading"
+          texto="Loading"
+          icone={<IconLoader />}
+          selecionado={url === '/loading'}
+        />
+        <MenuItem
+          url="/erro"
+          texto="Erro"
+          icone={<IconBug />}
+          selecionado={url === '/erro'}
+        />
       </MenuGrupo>
     </nav>
   );
